c25f5efd8a1700d878c08e1d9d9501e2a913bb10
Kevin Xiwei Zheng <blankplacement@gmail.com>
2019-06-09 16:31:28 -0700

    Bring back "continue reading" breaks with <details>

--- lib/default.rb
+++ lib/default.rb
@@ -1,5 +1,6 @@
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Capturing
+include Nanoc::Helpers::Filtering
 include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Text
