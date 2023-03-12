5b6576e12f486e5a5f148dc8b3a256f24eb57234
Kevin Xiwei Zheng <blankplacement@gmail.com>
2023-03-12 11:57:06 +0900

    Use `title_proc` for Atom feed

--- lib/summarize.rb
+++ lib/summarize.rb
@@ -4,34 +4,6 @@ module Room208
       item[:title] || excerptize(
         strip_html(item.compiled_content), length: 64, omission: '…')
     end
-
-    class SummaryTitlingItemProxy
-      instance_methods.each do |m|
-        undef_method(m) unless m =~ /(^__|^nil\?|^send$|^object_id$)/
-      end
-
-      def initialize(item)
-        @target = item
-      end
-
-      def respond_to?(symbol, include_priv=false)
-        @target.respond_to?(symbol, include_priv)
-      end
-
-      def [](object)
-        if object == :title
-          summarize(@target)
-        else
-          @target[object]
-        end
-      end
-
-      private
-
-      def method_missing(method, ...)
-        @target.send(method, ...)
-      end
-    end
   end
 end
 
